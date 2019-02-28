ba2f3ec7a516287d4c6ef5ea33e7a0ea5b15876c
wiktorlesiak <B00087701@student.itb.ie>
2019-02-28 11:18:07 +0000

    created ability for the object to be saved

--- datastore.py
+++ datastore.py
@@ -2,3 +2,9 @@ import ZODB, ZODB.FileStorage
 import persistent
 import transaction
 
+class Student(persistent.Persistent):
+    studentName = ''
+    def setStudentName(self, sName):
+        self.studentName = sName
+    def getStudentName(self):
+        return self.studentName
